68c3a2df6948a877b53223d812c246f79348a438
Nitin <nitins1020110@gmail.com>
2025-09-05 20:40:39 +0530

    29thquestion

---
+++ 29Array_Mathematics.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+n,m = map(int,input().split())
+A = [list(map(int, input().split())) for _ in range(n)]
+B = [list(map(int, input().split())) for _ in range(n)]
+a = np.array(A) 
+b = np.array(B)
+
+print(a+b,a-b,a*b,a//b,a%b,a**b,sep="\n")
